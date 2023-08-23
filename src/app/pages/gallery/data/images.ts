58b2fd6a28a277d0bddb3210449c39bf0206c165
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-23 23:47:38 +0300

    style : delete comments at gallery/data/images.ts

--- src/app/pages/gallery/data/images.ts
+++ src/app/pages/gallery/data/images.ts
@@ -3,18 +3,3 @@ import image2 from "../../../../../public/assets/content/slider/2-2x.jpg";
 import image3 from "../../../../../public/assets/content/slider/3-2x.jpg";
 
 export const images = [image1, image2, image3];
-
-// import {
-// 	default as image1,
-// 	default as image4,
-// } from "../../../../../public/assets/content/slider/1-2x.jpg";
-// import {
-// 	default as image2,
-// 	default as image5,
-// } from "../../../../../public/assets/content/slider/2-2x.jpg";
-// import {
-// 	default as image3,
-// 	default as image6,
-// } from "../../../../../public/assets/content/slider/3-2x.jpg";
-
-// export const images = [image1, image2, image3, image4, image5, image6];
