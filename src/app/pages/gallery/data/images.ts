6c9948332d056aee94304b84aca4fbff741fb1d2
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-08-24 09:04:43 +0300

    style : fix structure of assets and public folders
    - change imports according to the changes

--- src/app/pages/gallery/data/images.ts
+++ src/app/pages/gallery/data/images.ts
@@ -1,5 +1,5 @@
-import image1 from "../../../../../public/assets/content/slider/1-2x.jpg";
-import image2 from "../../../../../public/assets/content/slider/2-2x.jpg";
-import image3 from "../../../../../public/assets/content/slider/3-2x.jpg";
+import image1 from "@/app/assets/gallery/1-2x.jpg";
+import image2 from "@/app/assets/gallery/2-2x.jpg";
+import image3 from "@/app/assets/gallery/3-2x.jpg";
 
 export const images = [image1, image2, image3];
--- src/app/pages/services/Slider/data/images.ts
+++ src/app/pages/services/Slider/data/images.ts
@@ -1,7 +1,7 @@
-import image1 from "../../../../../../public/assets/content/section-3/1-2x.jpg";
-import image2 from "../../../../../../public/assets/content/section-3/2-2x.jpg";
-import image3 from "../../../../../../public/assets/content/section-3/3-2x.jpg";
-import image4 from "../../../../../../public/assets/content/section-3/4-2x.jpg";
-import image5 from "../../../../../../public/assets/content/section-3/5-2x.jpg";
+import image1 from "@/app/assets/services/1-2x.jpg";
+import image2 from "@/app/assets/services/2-2x.jpg";
+import image3 from "@/app/assets/services/3-2x.jpg";
+import image4 from "@/app/assets/services/4-2x.jpg";
+import image5 from "@/app/assets/services/5-2x.jpg";
 
 export const images = [image1, image2, image3, image4, image5];
